93746086b48dca42931ba40f0e41e35631acf63d
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-02-20 15:32:53 -0300

    initial commit

---
+++ src/main/java/com/xfef0/rabbitMQ/RabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.xfef0.rabbitMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xfef0/rabbitMQ/RabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xfef0.rabbitMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
