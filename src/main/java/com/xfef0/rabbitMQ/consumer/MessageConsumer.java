f96b5131685d1c144972258f7dee9c396f15b60d
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-02-21 16:42:29 -0300

    add message consumer

---
+++ src/main/java/com/xfef0/rabbitMQ/consumer/MessageConsumer.java
@@ -0,0 +1,19 @@
+package com.xfef0.rabbitMQ.consumer;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MessageConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
+    public void consume(String message) {
+        LOGGER.info("Message received: {}", message);
+    }
+}
