6ba0b5da71637ff5a5f818dc5daf90d91b768041
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-02-21 17:07:35 -0300

    add config and object to serialize/deserialize

--- src/main/java/com/xfef0/rabbitMQ/config/RabbitMQConfig.java
+++ src/main/java/com/xfef0/rabbitMQ/config/RabbitMQConfig.java
@@ -1,9 +1,10 @@
 package com.xfef0.rabbitMQ.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -49,5 +50,17 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(jsonQueue())
                 .to(exchange())
                 .with(jsonRoutingKey);
+    }
+
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
     }
 }
---
+++ src/main/java/com/xfef0/rabbitMQ/dto/User.java
@@ -0,0 +1,10 @@
+package com.xfef0.rabbitMQ.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+}
