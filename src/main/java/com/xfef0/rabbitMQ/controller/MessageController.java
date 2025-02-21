641b931aa7d4da80add30c138aa3a77b4dbc6c79
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-02-21 17:40:25 -0300

    add post /publish endpoint to send JSON message (user)

--- src/main/java/com/xfef0/rabbitMQ/controller/MessageController.java
+++ src/main/java/com/xfef0/rabbitMQ/controller/MessageController.java
@@ -1,12 +1,11 @@
 package com.xfef0.rabbitMQ.controller;
 
+import com.xfef0.rabbitMQ.dto.User;
+import com.xfef0.rabbitMQ.publisher.JsonPublisher;
 import com.xfef0.rabbitMQ.publisher.MessageProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,10 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     private final MessageProducer messageProducer;
+    private final JsonPublisher jsonMessageProducer;
 
     @GetMapping("/publish")
     public ResponseEntity<String> sendMessage(@RequestParam String message) {
         messageProducer.sendMessage(message);
         return ResponseEntity.ok("Message sent");
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user) {
+        jsonMessageProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("JSON message sent");
     }
 }
