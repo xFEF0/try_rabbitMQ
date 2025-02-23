01d309b656c47111a59ae7742024732483f46d06
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-02-23 16:10:09 -0300

    add POST /api/v1/publish test

--- src/test/java/com/xfef0/rabbitMQ/controller/MessageControllerTest.java
+++ src/test/java/com/xfef0/rabbitMQ/controller/MessageControllerTest.java
@@ -1,9 +1,13 @@
 package com.xfef0.rabbitMQ.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xfef0.rabbitMQ.dto.User;
+import com.xfef0.rabbitMQ.publisher.JsonPublisher;
 import com.xfef0.rabbitMQ.publisher.MessageProducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -16,21 +20,41 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(MessageController.class)
 class MessageControllerTest {
 
+    private static final String API_PREFIX = "/api/v1";
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     @MockitoBean
     private MessageProducer messageProducer;
+    @MockitoBean
+    private JsonPublisher jsonPublisher;
     @Autowired
     private MockMvc mockMvc;
-
 
     @Test
     void shouldSendMessage() throws Exception {
         String message = "testing publish endpoint";
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/publish")
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(API_PREFIX + "/publish")
                 .param("message", message));
 
         verify(messageProducer, times(1)).sendMessage(eq(message));
         resultActions.andExpect(status().isOk())
                 .andExpect(content().string("Message sent"));
+    }
+
+    @Test
+    void shouldSendJsonMessage() throws Exception {
+        User user = new User();
+        user.setId(1);
+        user.setLastName("Last");
+        user.setFirstName("First");
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(API_PREFIX + "/publish")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsString(user))
+        );
+
+        verify(jsonPublisher, times(1)).sendJsonMessage(eq(user));
+        resultActions.andExpect(status().isOk())
+                .andExpect(content().string("JSON message sent"));
     }
 
 }
